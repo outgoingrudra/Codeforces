aebbe335d13b16c38acc2b3abbbec116a85ad9a7
Rudra Verma <rudraverma781@gmail.com>
2026-01-10 19:58:12 +0530

    Again conquered !!!

---
+++ QueueAtTheSchool.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class QueueAtTheSchool {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        String s = sc.next();
+
+        char[] arr = s.toCharArray();
+
+        while (t-- > 0) {
+            for (int i = 0; i < n - 1; i++) {
+                if (arr[i] == 'B' && arr[i + 1] == 'G') {
+                    char temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    i++; // skip next index
+                }
+            }
+        }
+
+        System.out.println(new String(arr));
+        sc.close();
+    }
+}
