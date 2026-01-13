1019decb2e8162abb03ce80c921775961b868521
Rudra Verma <rudraverma781@gmail.com>
2026-01-13 20:14:12 +0530

    Again conquered !!!

---
+++ AntonAndDanik.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class AntonAndDanik {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String s = sc.next();
+
+        int anton = 0, danik = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'A') anton++;
+            else danik++;
+        }
+
+        if (anton > danik) {
+            System.out.println("Anton");
+        } else if (danik > anton) {
+            System.out.println("Danik");
+        } else {
+            System.out.println("Friendship");
+        }
+
+        sc.close();
+    }
+}
